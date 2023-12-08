fcbbc494dc0087be3e5bc5a8bb9377c82af4dbb9
David Forbes <davidforbesi@hotmail.com>
2023-12-08 15:48:07 +0000

    changed css import path after moving stylesheet into css folder

--- cv-builder-app/src/main.jsx
+++ cv-builder-app/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Forms from "./components/Forms.jsx";
 import CV from "./components/CV.jsx";
-import "./main.css";
+import "./css/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
