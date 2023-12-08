fd6c8c2b43ca1f37f92501d2008d416608c67859
David Forbes <davidforbesi@hotmail.com>
2023-12-08 15:48:42 +0000

    added some html/jsx for CV

--- cv-builder-app/src/components/CV.jsx
+++ cv-builder-app/src/components/CV.jsx
@@ -1,7 +1,39 @@
+import "../css/CV.css";
+
 export default function CV() {
   return (
     <div className="C-V">
-      <h1>CV</h1>
+      <header>
+        <h1>Your Name</h1>
+        <p>Curriculum Vitae</p>
+      </header>
+
+      <section>
+        <h2>Contact Information</h2>
+        <p>Email: your.email@example.com</p>
+        <p>Phone: (123) 456-7890</p>
+        <p>LinkedIn: linkedin.com/in/yourname</p>
+      </section>
+
+      <section>
+        <h2>Education</h2>
+        <h3>Degree in Something Awesome</h3>
+        <p>University Name, Graduation Year</p>
+      </section>
+
+      <section>
+        <h2>Work Experience</h2>
+        <h3>Job Title</h3>
+        <p>Company Name, Start Date - End Date</p>
+        <p>
+          Description of your responsibilities and achievements in this role.
+        </p>
+      </section>
+
+      <section>
+        <h2>Skills</h2>
+        <p>HTML, CSS, JavaScript, React, etc.</p>
+      </section>
     </div>
   );
 }
